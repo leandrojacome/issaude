c7c6c5e2c2d3f8dd9d84580c178464241fe4ba2d
Leandro Jácome <leandro.jacome@gmail.com>
2020-08-04 22:12:03 -0300

    Projeto criado

---
+++ src/main/java/br/com/isgh/issaude/IssaudeApplication.java
@@ -0,0 +1,13 @@
+package br.com.isgh.issaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssaudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/isgh/issaude/IssaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isgh.issaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
