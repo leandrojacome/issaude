6295a578f3ccc5d5e6d979c37b11df00b7699bc8
Leandro Jácome <leandro.jacome@gmail.com>
2020-08-04 22:58:09 -0300

    Testando o domínio

--- src/main/java/br/com/isgh/issaude/resources/ClinicaResource.java
+++ src/main/java/br/com/isgh/issaude/resources/ClinicaResource.java
@@ -1,16 +1,29 @@
 package br.com.isgh.issaude.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.isgh.issaude.domain.Clinica;
+
 
 @RestController
 @RequestMapping(value="/clinicas")
 public class ClinicaResource {
 
 	@GetMapping
-	public String list() {
-		return "REST está funcionando";
+	public List<Clinica> list() {
+		Clinica clin1 = new Clinica(1, "Clinica 1");
+		Clinica clin2 = new Clinica(2, "Clinica 2");
+		
+		List<Clinica> listaClinicas = new ArrayList<Clinica>();
+		listaClinicas.add(clin1);
+		listaClinicas.add(clin2);
+		
+		return listaClinicas;
 	}
 	
 }
